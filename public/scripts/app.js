c49e87717af5ea741007431383d11a7f49acc5d5
tgaff <tgaff@alumni.nd.edu>
2015-12-07 20:52:06 -0800

    Sprint 2 Step 2: form added, clears on submit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,7 +8,19 @@
 
 $(document).ready(function() {
   console.log('app.js loaded!');
-  renderAlbum(sampleAlbums[0]);
+  $.get('/api/albums').success(function (albums) {
+    albums.forEach(function(album) {
+      renderAlbum(album);
+    });
+  });
+
+  $('#album-form form').on('submit', function(e) {
+    e.preventDefault();
+    var formData = $(this).serialize();
+    console.log('formData', formData);
+    $(this).trigger("reset");
+  });
+
 });
 
 
